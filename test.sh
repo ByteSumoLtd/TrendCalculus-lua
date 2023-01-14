22d55a3dd12c9304a8e8900968faf107023ee4f6
Andrew Morgan <andrew@bytesymo.com>
2023-01-14 02:55:16 +0000

    updated test.sh to include series name in outputs

--- test.sh
+++ test.sh
@@ -13,41 +13,40 @@ p=2  # set this to 2 so we can build, bottom-up, the levelling in the B-Tree of 
 
 src="data/"    # this is the directory for input data in trendcalculus-public
 
-rm out/*.csv
-
+rm out/*rev*.csv
 name="all_DJI"
 
 cp ${src}db_output_${name}.csv db_output.csv
 
 time cat db_output.csv |  lua trendcalculus.lua -F "," -OFS "," -p 1 -h -H -n $z -r \
-	| tee out/rev01.csv |  lua trendcalculus.lua -F "," -OFS "," -p 2 -h -H -n $p -r \
-        | tee out/rev02.csv | lua trendcalculus.lua -F "," -OFS "," -p 3 -h -H -n $p -r  \
-        | tee out/rev03.csv | lua trendcalculus.lua -F "," -OFS "," -p 4 -h -H -n $p -r  \
-        | tee out/rev04.csv | lua trendcalculus.lua -F "," -OFS "," -p 5 -h -H -n $p -r  \
-        | tee out/rev05.csv | lua trendcalculus.lua -F "," -OFS "," -p 6 -h -H -n $p -r  \
-        | tee out/rev06.csv | lua trendcalculus.lua -F "," -OFS "," -p 7 -h -H -n $p -r  \
-        | tee out/rev07.csv | lua trendcalculus.lua -F "," -OFS "," -p 8 -h -H -n $p -r  \
-        | tee out/rev08.csv | lua trendcalculus.lua -F "," -OFS "," -p 9 -h -H -n $p -r  \
-        | tee out/rev09.csv | lua trendcalculus.lua -F "," -OFS "," -p 10 -h -H -n $p -r  \
-        | tee out/rev10.csv | lua trendcalculus.lua -F "," -OFS "," -p 11 -h -H -n $p -r  \
-        | tee out/rev11.csv | lua trendcalculus.lua -F "," -OFS "," -p 12 -h -H -n $p -r  \
-        | tee out/rev12.csv | lua trendcalculus.lua -F "," -OFS "," -p 13 -h -H -n $p -r  \
-	| tee out/rev13.csv | lua trendcalculus.lua -F "," -OFS "," -p 14 -h -H -n $p -r > out/rev14.csv
+	| tee out/${name}_rev01.csv |  lua trendcalculus.lua -F "," -OFS "," -p 2 -h -H -n $p -r \
+        | tee out/${name}_rev02.csv | lua trendcalculus.lua -F "," -OFS "," -p 3 -h -H -n $p -r  \
+        | tee out/${name}_rev03.csv | lua trendcalculus.lua -F "," -OFS "," -p 4 -h -H -n $p -r  \
+        | tee out/${name}_rev04.csv | lua trendcalculus.lua -F "," -OFS "," -p 5 -h -H -n $p -r  \
+        | tee out/${name}_rev05.csv | lua trendcalculus.lua -F "," -OFS "," -p 6 -h -H -n $p -r  \
+        | tee out/${name}_rev06.csv | lua trendcalculus.lua -F "," -OFS "," -p 7 -h -H -n $p -r  \
+        | tee out/${name}_rev07.csv | lua trendcalculus.lua -F "," -OFS "," -p 8 -h -H -n $p -r  \
+        | tee out/${name}_rev08.csv | lua trendcalculus.lua -F "," -OFS "," -p 9 -h -H -n $p -r  \
+        | tee out/${name}_rev09.csv | lua trendcalculus.lua -F "," -OFS "," -p 10 -h -H -n $p -r  \
+        | tee out/${name}_rev10.csv | lua trendcalculus.lua -F "," -OFS "," -p 11 -h -H -n $p -r  \
+        | tee out/${name}_rev11.csv | lua trendcalculus.lua -F "," -OFS "," -p 12 -h -H -n $p -r  \
+        | tee out/${name}_rev12.csv | lua trendcalculus.lua -F "," -OFS "," -p 13 -h -H -n $p -r  \
+	| tee out/${name}_rev13.csv | lua trendcalculus.lua -F "," -OFS "," -p 14 -h -H -n $p -r > out/${name}_rev14.csv
 
  time cat db_output.csv |  lua trendcalculus.lua -F "," -OFS "," -p 1 -h -H -n $z -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev01.csv |  lua trendcalculus.lua -F "," -OFS "," -p 2 -h -H -n $p -s| lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev02.csv | lua trendcalculus.lua -F "," -OFS "," -p 3 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev03.csv | lua trendcalculus.lua -F "," -OFS "," -p 4 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev04.csv | lua trendcalculus.lua -F "," -OFS "," -p 5 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev05.csv | lua trendcalculus.lua -F "," -OFS "," -p 6 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev06.csv | lua trendcalculus.lua -F "," -OFS "," -p 7 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev07.csv | lua trendcalculus.lua -F "," -OFS "," -p 8 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev08.csv | lua trendcalculus.lua -F "," -OFS "," -p 9 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev09.csv | lua trendcalculus.lua -F "," -OFS "," -p 10 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev10.csv | lua trendcalculus.lua -F "," -OFS "," -p 11 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev11.csv | lua trendcalculus.lua -F "," -OFS "," -p 12 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev12.csv | lua trendcalculus.lua -F "," -OFS "," -p 13 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
-        | tee out/srev13.csv | lua trendcalculus.lua -F "," -OFS "," -p 14 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS "," > out/srev14.csv
+        | tee out/${name}_srev01.csv |  lua trendcalculus.lua -F "," -OFS "," -p 2 -h -H -n $p -s| lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev02.csv | lua trendcalculus.lua -F "," -OFS "," -p 3 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev03.csv | lua trendcalculus.lua -F "," -OFS "," -p 4 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev04.csv | lua trendcalculus.lua -F "," -OFS "," -p 5 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev05.csv | lua trendcalculus.lua -F "," -OFS "," -p 6 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev06.csv | lua trendcalculus.lua -F "," -OFS "," -p 7 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev07.csv | lua trendcalculus.lua -F "," -OFS "," -p 8 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev08.csv | lua trendcalculus.lua -F "," -OFS "," -p 9 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev09.csv | lua trendcalculus.lua -F "," -OFS "," -p 10 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev10.csv | lua trendcalculus.lua -F "," -OFS "," -p 11 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev11.csv | lua trendcalculus.lua -F "," -OFS "," -p 12 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev12.csv | lua trendcalculus.lua -F "," -OFS "," -p 13 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS ","  \
+        | tee out/${name}_srev13.csv | lua trendcalculus.lua -F "," -OFS "," -p 14 -h -H -n $p -s | lua rev.lua -F "," -h -H -s -OFS "," > out/${name}_srev14.csv
 
 
 mv db_output.csv out/.
